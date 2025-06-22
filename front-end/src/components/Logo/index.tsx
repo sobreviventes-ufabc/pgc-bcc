bc6ca73a61d5ea1cec4ff5a3810b62e46887ebd7
MatheusR42 <matheus.araujo.r42@gmail.com>
2025-06-21 23:25:48 -0300

    fix image performance

--- front-end/src/components/Logo/index.tsx
+++ front-end/src/components/Logo/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const Logo: React.FC = () => {
-    return <Image src="/img/logo.svg" className="app-logo" alt="UFABChat Logo" width={268} height={74} />;
+    return <Image src="/img/logo.svg" className="app-logo" alt="UFABChat Logo" width={268} height={74} priority />;
 };
 
 export default Logo;
