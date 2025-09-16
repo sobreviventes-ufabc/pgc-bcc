4ad919302e22e4082e9a0c0fa61d550b47d15b01
Aline Milene <alinem2104@gmail.com>
2025-09-15 21:41:52 -0300

    removendo arquivo de setup obsoleto

--- data_extraction/scripts/unstructured/setup.py
+++
@@ -1,9 +0,0 @@
-# rodar no terminal windows: 
-
-# python -m pip install pytesseract pdf2image python-magic-bin 
-# python -m pip install -Uq "unstructured[all-docs]" pillow lxml pillow
-# python -m pip install -Uq chromadb tiktoken
-# python -m pip install -Uq langchain langchain-community langchain-openai langchain-groq
-# python -m pip install -Uq python_dotenv
-# python -m pip install langchain langchain-openai langchain-groq chromadb unstructured pdf2image pillow
-# python -m pip install -U langchain-ollama
