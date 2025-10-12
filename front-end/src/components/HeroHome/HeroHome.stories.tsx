814f8fd3540e594cdb2d8d336d59e956a144269f
MatheusR42 <matheus.araujo.r42@gmail.com>
2025-10-12 03:44:36 -0300

    fix hero home stories

--- front-end/src/components/HeroHome/HeroHome.stories.tsx
+++ front-end/src/components/HeroHome/HeroHome.stories.tsx
@@ -1,13 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
-
-
+import type { Decorator } from '@storybook/nextjs-vite';
+import React from 'react';
 
 import HeroHome from './index';
+import { ChatProvider } from '@/context/ChatContext';
+
+const withChatProvider: Decorator = (Story) => (
+  <ChatProvider>
+    <Story />
+  </ChatProvider>
+);
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Components/HeroHome',
   component: HeroHome,
+  decorators: [withChatProvider],
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
